0f561b3d72059c97fd59224e8e7451e8a238c9e0
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2024-11-06 00:18:07 +0700

    menambahkan logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 export default function Home() {
   
   return (
@@ -6,7 +8,13 @@ export default function Home() {
       <nav className="bg-white p-4 shadow">
         <ul className="flex space-x-4">
           <li>
-            <a href="#" className="text-gray-800 hover:text-gray-600">GAMAFORCE</a>
+            {/* <a href="#" className="text-gray-800 hover:text-gray-600">GAMAFORCE</a> */}
+            <Image
+                src="/images/logo-gamaforce.png" // Path to the logo image
+                alt="Gamaforce Logo"
+                width={150} // Set width of the logo
+                height={50} // Set height of the logo
+              />
           </li>
         </ul>
       </nav>
