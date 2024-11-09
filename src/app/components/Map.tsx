2afc90643322cb859966a2354433147578d9ed12
diankartika <diankartika487@gmail.com>
2024-11-09 22:05:25 +0700

    misah navbar

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import { useState, useEffect } from "react";
 
 const Map = () => {
-  const [height, setHeight] = useState("100vh"); 
+  const [height, setHeight] = useState("100vh");
 
   useEffect(() => {
     // Set initial height and adjust on window resize
-    const updateHeight = () => setHeight(`${window.innerHeight - 150}px`); 
+    const updateHeight = () => setHeight(`${window.innerHeight - 150}px`);
 
     updateHeight();
     window.addEventListener("resize", updateHeight);
@@ -17,9 +18,16 @@ const Map = () => {
     return () => window.removeEventListener("resize", updateHeight);
   }, []);
 
+  // Buat ikon khusus untuk logo UGM
+  const ugmIcon = L.icon({
+    iconUrl: "/images/logo-ugm.png", // Path logo UGM di folder public
+    iconSize: [50, 50], // Sesuaikan ukuran ikon
+    iconAnchor: [25, 50], // Menentukan titik tengah bawah gambar sebagai titik jangkar
+  });
+
   return (
     <MapContainer
-      style={{ height: height, width: "100%" }} 
+      style={{ height: height, width: "100%" }}
       center={[-7.770717, 110.377945]}
       zoom={17}
       scrollWheelZoom={false}
@@ -28,6 +36,9 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {/* Tambahkan Marker dengan ikon UGM */}
+      <Marker position={[-7.770717, 110.377945]} icon={ugmIcon} />
     </MapContainer>
   );
 };
