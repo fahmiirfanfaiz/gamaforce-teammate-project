9565f2ce310adb80be9dd581e5c84f06899e461c
diankartika <diankartika487@gmail.com>
2024-11-10 14:11:46 +0700

    menambahkan marker almost selesai

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,16 +1,23 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useState, useEffect } from "react";
+import "leaflet-draw/dist/leaflet.draw.css"
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+});
 
 const Map = () => {
   const [height, setHeight] = useState("100vh");
 
   useEffect(() => {
     const updateHeight = () => setHeight(`${window.innerHeight - 150}px`); 
-
     updateHeight();
     window.addEventListener("resize", updateHeight);
 
@@ -23,6 +30,8 @@ const Map = () => {
     iconAnchor: [25, 50], 
   });
 
+  const _created = (e: any) => console.log(e);
+
   return (
     <MapContainer
       style={{ height: height, width: "100%" }}
@@ -30,6 +39,24 @@ const Map = () => {
       zoom={17}
       scrollWheelZoom={false}
     >
+      <FeatureGroup>
+        <div className="relative mt-[-2vw]">
+          <EditControl
+            position="bottomright"
+            //className=""
+            onCreated={_created}
+            draw={
+              {
+                rectangle: true,
+                circle: true,
+                circlemarker: true,
+                marker: true,
+                polyline: true,
+                }
+            }
+          />
+        </div>
+      </FeatureGroup>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
