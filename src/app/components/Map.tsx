c770dd72e7065547fd5c5e75b62aa72461d62334
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2024-11-10 14:48:15 +0700

    menyelesaikan marker

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,71 +1,71 @@
-"use client";
+  "use client";
 
-import { MapContainer, TileLayer, Marker, FeatureGroup } from "react-leaflet";
-import { EditControl } from "react-leaflet-draw";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import { useState, useEffect } from "react";
-import "leaflet-draw/dist/leaflet.draw.css"
+  import { MapContainer, TileLayer, Marker, FeatureGroup } from "react-leaflet";
+  import { EditControl } from "react-leaflet-draw";
+  import "leaflet/dist/leaflet.css";
+  import L from "leaflet";
+  import { useState, useEffect } from "react";
+  import "leaflet-draw/dist/leaflet.draw.css"
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
-  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
-});
-
-const Map = () => {
-  const [height, setHeight] = useState("100vh");
-
-  useEffect(() => {
-    const updateHeight = () => setHeight(`${window.innerHeight - 150}px`); 
-    updateHeight();
-    window.addEventListener("resize", updateHeight);
-
-    return () => window.removeEventListener("resize", updateHeight);
-  }, []);
-
-  const ugmIcon = L.icon({
-    iconUrl: "/images/logo-ugm.png", 
-    iconSize: [50, 50], 
-    iconAnchor: [25, 50], 
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
   });
 
-  const _created = (e: any) => console.log(e);
+  const Map = () => {
+    const [height, setHeight] = useState("100vh");
 
-  return (
-    <MapContainer
-      style={{ height: height, width: "100%" }}
-      center={[-7.770717, 110.377945]}
-      zoom={17}
-      scrollWheelZoom={false}
-    >
-      <FeatureGroup>
-        <div className="relative mt-[-2vw]">
-          <EditControl
-            position="bottomright"
-            //className=""
-            onCreated={_created}
-            draw={
-              {
-                rectangle: true,
-                circle: true,
-                circlemarker: true,
-                marker: true,
-                polyline: true,
-                }
-            }
-          />
-        </div>
-      </FeatureGroup>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    useEffect(() => {
+      const updateHeight = () => setHeight(`${window.innerHeight - 150}px`); 
+      updateHeight();
+      window.addEventListener("resize", updateHeight);
 
-      {/* Tambahkan Marker dengan ikon UGM */}
-      <Marker position={[-7.770717, 110.377945]} icon={ugmIcon} />
-    </MapContainer>
-  );
-};
+      return () => window.removeEventListener("resize", updateHeight);
+    }, []);
 
-export default Map;
+    const ugmIcon = L.icon({
+      iconUrl: "/images/logo-ugm.png", 
+      iconSize: [50, 50], 
+      iconAnchor: [25, 50], 
+    });
+
+    const _created = (e: any) => console.log(e);
+
+    return (
+      <MapContainer
+        style={{ height: height, width: "100%" }}
+        center={[-7.770717, 110.377945]}
+        zoom={17}
+        scrollWheelZoom={false}
+      >
+        <FeatureGroup>
+          <div className="fixed">
+            <EditControl
+              position="topright"
+              // className="fixed bottom-4 right-4 z-5"
+              onCreated={_created}
+              draw={
+                {
+                  rectangle: true,
+                  circle: true,
+                  circlemarker: true,
+                  marker: true,
+                  polyline: true,
+                  }
+              }
+            />
+          </div>
+        </FeatureGroup>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {/* Tambahkan Marker dengan ikon UGM */}
+        <Marker position={[-7.770717, 110.377945]} icon={ugmIcon} />
+      </MapContainer>
+    );
+  };
+
+  export default Map;
